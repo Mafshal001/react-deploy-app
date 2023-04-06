0db3f4592aba2b11f475c13a6f7ee54c3f6833a9
m.afshal <m.afshal@xintsolutions.com>
2023-04-06 05:08:58 +0000

    updated

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Teri Behn di Siri
         </a>
       </header>
     </div>
